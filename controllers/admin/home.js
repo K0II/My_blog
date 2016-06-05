716e40f0175732ec1c53c96147aa36955af5d4c3
KOI <1428757462@qq.com>
2016-06-05 20:24:01 +0800

    调整样式

--- controllers/admin/home.js
+++ controllers/admin/home.js
@@ -9,5 +9,37 @@ module.exports = {
             }
             res.render('admin/home',{layout:'admin',data:viewArticles(articles)});
         });
+    },
+    retrieve_post: function(req,res){
+        var reg_title =  eval("/.*" + req.body.title + ".*/");
+        var reg_entity = eval("/.*" + req.body.entity + ".*/");
+        var reg_biblio = eval("/.*" + req.body.biblio + ".*/");
+        articles.find({
+            title: reg_title,
+            entity: reg_entity,
+            biblio: reg_biblio,
+        },function(err,articles){
+            if(err){
+                console.error(err.stack);
+            }
+            else {
+                res.render('admin/home',{layout:'admin',data:viewArticles(articles)});
+            }
+        })
+    },
+    delete_run: function(req,res){
+        articles.remove({_id: req.params.id},function(err,docs){
+            if(err) {
+                console.error(err.stack);
+            }
+            else{
+                articles.find({},function(err,articles){
+                    if(err){
+                        console.error(err.stack);
+                    }
+                    res.render('admin/home',{layout:'admin',data:viewArticles(articles)});
+                });
+            }
+        });
     }
 }
